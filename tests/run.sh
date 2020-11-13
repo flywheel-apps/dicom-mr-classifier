e1b908f8689ea51b39d9ee9e992ae35773e46678
Nate Richman <naterichman@flywheel.io>
2020-11-13 10:02:48 -0600

    MAIN: Change to docker build dir

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build .. -t flywheel/dicom-mr-classifier:2.0.0
-docker run -it --entrypoint=/bin/bash -v $(pwd)/T1w.dicom.zip:/flywheel/v0/input/dicom/T1w.dicom.zip -v $(pwd)/config.json:/flywheel/v0/config.json flywheel/dicom-mr-classifier:2.0.0
+cd ..
+docker build . -t flywheel/dicom-mr-classifier:2.0.0
+docker run -it --entrypoint=/bin/bash -v $(pwd)/tests/T1w.dicom.zip:/flywheel/v0/input/dicom/T1w.dicom.zip -v $(pwd)/tests/config.json:/flywheel/v0/config.json flywheel/dicom-mr-classifier:2.0.0
