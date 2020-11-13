4f8a378a8ffa1de5e0c4aabc2470706455bf5c6d
naterichman <68390038+naterichman@users.noreply.github.com>
2020-11-13 10:25:32 -0600

    Bump version in run test script

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
-docker build . -t flywheel/dicom-mr-classifier:2.0.0
-docker run -it --entrypoint=/bin/bash -v $(pwd)/tests/T1w.dicom.zip:/flywheel/v0/input/dicom/T1w.dicom.zip -v $(pwd)/tests/config.json:/flywheel/v0/config.json flywheel/dicom-mr-classifier:2.0.0
+docker build . -t scitran/dicom-mr-classifier:2.0.0
+docker run -it --entrypoint=/bin/bash -v $(pwd)/tests/T1w.dicom.zip:/flywheel/v0/input/dicom/T1w.dicom.zip -v $(pwd)/tests/config.json:/flywheel/v0/config.json scitran/dicom-mr-classifier:2.0.0
